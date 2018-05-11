581c08b99434a1fdfaf32bdaeef1ce0ec06aff89
Abhishek Jain <abhisheknjain11@gmail.com>
2018-05-11 15:00:08 +0530

    New formula add in horses

--- horses.php
+++ horses.php
@@ -143,3 +143,87 @@ if ($result1->num_rows > 0) {
         });
     </script>
 
+
+<?php
+function newvalue($length,$distance,$orgdistance,$pos,$time){
+    $modifier = 0;
+     //Getting the postion of the horse
+$pos =  explode('/', $pos);
+    		$position =  intval($pos[0]);
+               
+          //Getting the value of the modifier      
+if ($distance >= 800 AND $distance <= 999)
+	    {
+	    	$modifier = 1;
+	    }
+	    elseif ($distance >= 1000 AND $distance <= 1099)
+	    {
+	    	$modifier = 0.5;
+	    }
+	    elseif ($distance >= 1100 AND $distance <= 4000)
+	    {
+	    	$modifier = 0.7;
+	    }
+            $remainder = get_remainder($distance);
+        //if horse wins    
+            if($position==1){
+                if($distance>$orgdistance){
+                    win_rounded_up($time, $length, $modifier, $remainder);
+                }else{ 
+                    win_rounded_down($time, $length, $modifier, $remainder);
+                }
+            }else{
+                 if($distance>$orgdistance){
+                    loses_rounded_up($time, $length, $modifier, $remainder);
+                }else{ 
+                    loses_rounded_down($time, $length, $modifier, $remainder);
+                }
+            }
+            
+            
+}
+function get_remainder($distance){
+    if ($distance % 10 < 5)
+		{
+			$distance -= $distance % 10;
+                       
+		}
+		else
+		{
+			$distance += (10 - ($distance % 10));
+                       
+		}
+	    
+		if ($distance % 100 < 50)
+		{
+			$reminder_distance = $distance % 100;
+			
+                       
+		}
+		else
+		{
+			$reminder_distance = (100 - ($distance % 100));
+			
+                        
+		}
+                $reminder = $reminder_distance*0.01;
+                return $reminder;
+}
+
+
+function win_rounded_up($time,$length,$modifier,$remainder){
+    
+}
+function win_rounded_down($time,$length,$modifier,$remainder){
+    
+    
+}
+function loses_rounded_up($time,$length,$modifier,$remainder){
+    
+}
+
+function loses_rounded_down($time,$length,$modifier,$remainder){
+    
+}
+
+?>
