18f7750abd2b3d44b5cd6e7d2576ae0c5ac5834f
sidorkin-yegor <sidorkin.android.ad@gmail.com>
2018-05-15 05:52:43 +0300

    Added race_id to results

--- results.php
+++ results.php
@@ -36,7 +36,18 @@ $tables = $html->find('table.meeting');
 foreach ($tables as $table) {
     $rows = $table->find('tr');
     foreach ($rows as $row) {
-        $race_event = $row->find('td', 0)->find('a', 0)->plaintext;
+        // select meeting_id
+        $meeting_date = $today_date;
+        $meeting_name = $row->find('td', 0)->find('a', 0)->plaintext;
+        $stmt = $dbh->prepare("SELECT meeting_id FROM meetings WHERE meeting_date = ? AND meeting_name = ?");
+        if ($stmt->execute(array($meeting_date, $meeting_name))) {
+            $meeting_id = $stmt->fetchColumn();
+        } else {
+            $msg = "[" . date("Y-m-d H:i:s") . "] Find meeting_id error: " . $stmt->error;
+            echo $msg . "\n";
+        }
+        // .select meeting_id        
+        $race_number = 1;
         $tds = $row->find('td.popup-race');
         foreach ($tds as $td) {
             if (empty($td->title)) {
@@ -45,12 +56,22 @@ foreach ($tables as $table) {
             $link = $td->find('a', 0)->href;
             $race_link = $base_url.$link;
             $race_html = file_get_html($race_link);
-            
+            // get race_distance
             $race_distance_spans = $race_html->find('div#container > div > h1 > span');
             $race_distance_span = end($race_distance_spans);
             $race_distance = $race_distance_span->plaintext;
             $race_distance = str_ireplace('m', '', $race_distance);
-            
+            // .get race_distance
+            // select race_id
+            $stmt = $dbh->prepare("SELECT race_id FROM races WHERE meeting_id = ? AND race_number = ? AND race_distance = ?");
+            if ($stmt->execute(array($meeting_id, $race_number, $race_distance))) {
+                $race_id = $stmt->fetchColumn();
+            } else {
+                $msg = "[" . date("Y-m-d H:i:s") . "] Find race_id error: " . $stmt->error;
+                echo $msg . "\n";
+            }
+            $race_number++;
+            // .select race_id            
             $race_table = $race_html->find('table.formguide', 0);
             $race_table_rows = $race_table->find('tr');
             $i = 1;
@@ -59,13 +80,13 @@ foreach ($tables as $table) {
                     $horse_position = $i;
                     $horse_name = $race_table_row->find('td.horse a', 0)->plaintext;
                     $i++;
-
-                    $stmt = $dbh->prepare('INSERT INTO results (position, horse, date, event, distance) VALUE(:position, :horse, :date, :event, :distance)');
+                    $stmt = $dbh->prepare('INSERT INTO results (race_id, position, horse, date, event, distance) VALUE(:race_id, :position, :horse, :date, :event, :distance)');
                     $data = array(
+                        ':race_id' => $race_id,
                         ':position' => $horse_position,
                         ':horse' => $horse_name,
                         ':date' => $today_date_for_db,
-                        ':event' => $race_event,
+                        ':event' => $meeting_name,
                         ':distance' => $race_distance
                     );
                     if (!$stmt->execute($data)) {
