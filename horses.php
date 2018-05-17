3ea38f3e5f15c3e787f806e8ba2b8bb750e6a744
Abhishek Jain <abhisheknjain11@gmail.com>
2018-05-17 21:48:18 +0530

    New Results
    Added New Results as Discussed

--- horses.php
+++ horses.php
@@ -5,7 +5,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 session_start();
-if(!isset($_SESSION['mname'])){
+if (!isset($_SESSION['mname'])) {
     $_SESSION['mname'] = $_REQUEST['mname'];
 }
 $raceid = $_REQUEST['raceid'];
@@ -14,14 +14,14 @@ $raceid = $_REQUEST['raceid'];
 //$sql = "SELECT *, MIN(time) minimumtime,AVG(time) avgtime FROM data WHERE `name` IN (";
 $sql = "SELECT * , MIN(data.time) minimumtime,MIN(data.time2) minimumtime2 FROM horses LEFT JOIN data ON horses.horse_name = data.name WHERE horses.race_id =" . $raceid;
 
-$sql .=  " GROUP BY name,`distance`";
+$sql .= " GROUP BY name,`distance`";
 
 $result = $conn->query($sql);
 
 
 $meetingid = $_REQUEST['meetingid'];
 //$sql = "SELECT *, MIN(time) minimumtime,AVG(time) avgtime FROM data WHERE `name` IN (";
-$sql1 = "SELECT *  FROM races WHERE meeting_id =" . $meetingid." ORDER by race_id";
+$sql1 = "SELECT *  FROM races WHERE meeting_id =" . $meetingid . " ORDER by race_id";
 $result1 = $conn->query($sql1);
 ?>
 <!DOCTYPE html>
@@ -31,50 +31,50 @@ $result1 = $conn->query($sql1);
         <title>Horses Data</title>
         <link rel="stylesheet" id="font-awesome-style-css" href="http://phpflow.com/code/css/bootstrap3.min.css" type="text/css" media="all">
         <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css"/>
 
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
-  
 
-<link rel="stylesheet" id="main-css" href="main.css" type="text/css" media="all">
 
-<ul> <li><a href="meeting.php">Home</a></li>
-      <li><a href="meeting.php">Meetings</a></li>
-       <li><a href="result.php" >Results</a></li>
-      <li><a class="active"><?php echo $_SESSION['mname']; ?></a></li>
+        <link rel="stylesheet" id="main-css" href="main.css" type="text/css" media="all">
 
-      <?php
-if ($result1->num_rows > 0) {
-    // output data of each row
-    while ($row = $result1->fetch_assoc()) {
+    <ul> <li><a href="meeting.php">Home</a></li>
+        <li><a href="meeting.php">Meetings</a></li>
+        <li><a href="result.php" >Results</a></li>
+        <li><a class="active"><?php echo $_SESSION['mname']; ?></a></li>
+
+        <?php
+        if ($result1->num_rows > 0) {
+            // output data of each row
+            while ($row = $result1->fetch_assoc()) {
+                ?>
+                <li><a href=horses.php?raceid=<?php echo $row['race_id'] ?>&meetingid=<?php echo $meetingid; ?>&rd=<?php echo $row['race_distance'] ?> <?php if ($row['race_id'] == $_REQUEST['raceid']) { ?>class="active" <?php } ?>><?php echo $row['race_number'] ?></a></li>
+
+
+
+                <?php
+            }
+        }
         ?>
-<li><a href=horses.php?raceid=<?php echo $row['race_id'] ?>&meetingid=<?php echo $meetingid; ?>&rd=<?php echo $row['race_distance'] ?> <?php if($row['race_id']==$_REQUEST['raceid']){ ?>class="active" <?php } ?>><?php echo $row['race_number'] ?></a></li>
-  
-
-     
- <?php
-      
-}}
-    ?>
-</ul>
+    </ul>
     <div class="container-fluid">
         <div class="">
-            <h1>Horses Data - Distance <?php echo $_REQUEST['rd']; ?></h1>
+            <h1>Horses Data - Distance <?php echo $_REQUEST['rd']; ?>  <a href=races.php?race_id=<?php echo $raceid; ?>>View Results</a></h1>
             <div class="">
                 <table id="employee_grid" class="display" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>Horse No.</th>
                             <th>Horse Name</th>
-                             <th>Odds</th>
+                            <th>Odds</th>
                             <th>H2H</th>
                             <th>Position</th>
                             <th>Length</th>
@@ -86,7 +86,7 @@ if ($result1->num_rows > 0) {
                             <th>Sectional</th>
                             <th>Minimum Time</th>
                             <th>Handicap</th>
-                        
+
                         </tr>
                     </thead>
                     <tbody>
@@ -94,27 +94,27 @@ if ($result1->num_rows > 0) {
                         if ($result->num_rows > 0) {
                             // output data of each row
                             while ($row = $result->fetch_assoc()) {
-                                  $distance = round($row["original_distance"]/100);
-                                $distance = $distance*100;
-                                $newhandicap = newvalue($row["length"], $row["original_distance"], $distance, $row["pos"], number_format($row["minimumtime"],2));
-                                
-                               // $newhandicap = newvalue($row["length"], $row["original_distance"], $row["distance"], $row["pos"], number_format($row["minimumtime"],2));
-                                
+                                $distance = round($row["original_distance"] / 100);
+                                $distance = $distance * 100;
+                                $newhandicap = newvalue($row["length"], $row["original_distance"], $distance, $row["pos"], number_format($row["minimumtime"], 2));
+
+                                // $newhandicap = newvalue($row["length"], $row["original_distance"], $row["distance"], $row["pos"], number_format($row["minimumtime"],2));
+
                                 echo "<tr>"
                                 . "<td>" . $row["horse_number"] . "</td>"
                                 . "<td>" . $row["horse_name"] . "</td>"
                                 . "<td>" . $row["horse_fixed_odds"] . "</td>"
-                                         . "<td>" . $row["horse_h2h"] . "</td>"
-                                         . "<td>" . $row["pos"] . "</td>"
-                                         . "<td>" . $row["length"] . "</td>"
-                                         . "<td>" . $row["condition"] . "</td>"
-                                         . "<td>" . $row["original_distance"] . "</td>"
-                                         . "<td>" . $distance . "</td>"
-                                         . "<td>" . $row["weight"] . "</td>"
-                                          . "<td>" . $row["horse_weight"] . "</td>"
-                                         . "<td>" . $row["sectional"] . "</td>"
-                                         . "<td>" . $row["minimumtime"] . "</td>"
-                                         . "<td>" . number_format($newhandicap,3) . "</td>"
+                                . "<td>" . $row["horse_h2h"] . "</td>"
+                                . "<td>" . $row["pos"] . "</td>"
+                                . "<td>" . $row["length"] . "</td>"
+                                . "<td>" . $row["condition"] . "</td>"
+                                . "<td>" . $row["original_distance"] . "</td>"
+                                . "<td>" . $distance . "</td>"
+                                . "<td>" . $row["weight"] . "</td>"
+                                . "<td>" . $row["horse_weight"] . "</td>"
+                                . "<td>" . $row["sectional"] . "</td>"
+                                . "<td>" . $row["minimumtime"] . "</td>"
+                                . "<td>" . number_format($newhandicap, 3) . "</td>"
                                 . "</tr>";
                             }
                         } else {
@@ -125,9 +125,9 @@ if ($result1->num_rows > 0) {
                     </tbody>
                     <tfoot>
                         <tr>
-                              <th>Horse No.</th>
+                            <th>Horse No.</th>
                             <th>Horse Name</th>
-                             <th>Odds</th>
+                            <th>Odds</th>
                             <th>H2H</th>
                             <th>Position</th>
                             <th>Length</th>
@@ -139,7 +139,7 @@ if ($result1->num_rows > 0) {
                             <th>Sectional</th>
                             <th>Minimum Time</th>
                             <th>Handicap</th>
-                             <th>NewTime</th>
+                            <th>NewTime</th>
                         </tr>
                     </tfoot>
                 </table>
@@ -158,108 +158,94 @@ if ($result1->num_rows > 0) {
     </script>
 
 
-<?php
+    <?php
 
-function newvalue($length,$distance,$orgdistance,$pos,$time){
-    $modifier = 0;
-     //Getting the postion of the horse
-$pos =  explode('/', $pos);
-    		$position =  intval($pos[0]);
-               
-          //Getting the value of the modifier      
-/*if ($distance >= 800 AND $distance <= 999)
-	    {
-	    	$modifier = 1;
-	    }
-	    elseif ($distance >= 1000 AND $distance <= 1099)
-	    {
-	    	$modifier = 0.05;
-	    }
-	    elseif ($distance >= 1100 AND $distance <= 4000)
-	    {
-	    	$modifier = 0.07;
-	    }*/
-            $modifier = 0.03;
-            $remainder = get_remainder($distance);
-    
-         
-            
-        
-            if($position==1){
-                
-                if($distance<$orgdistance){
-                    
-                    $newtime =   win_rounded_up($time, $length, $modifier, $remainder);
-                }else{ 
-                $newtime =      win_rounded_down($time, $length, $modifier, $remainder);
-                }
-            }else{
-                 if($distance<$orgdistance){
-               $newtime =       loses_rounded_up($time, $length, $modifier, $remainder);
-                }else{ 
-              $newtime =        loses_rounded_down($time, $length, $modifier, $remainder);
-                }
+    function newvalue($length, $distance, $orgdistance, $pos, $time) {
+        $modifier = 0;
+        //Getting the postion of the horse
+        $pos = explode('/', $pos);
+        $position = intval($pos[0]);
+
+        //Getting the value of the modifier      
+        /* if ($distance >= 800 AND $distance <= 999)
+          {
+          $modifier = 1;
+          }
+          elseif ($distance >= 1000 AND $distance <= 1099)
+          {
+          $modifier = 0.05;
+          }
+          elseif ($distance >= 1100 AND $distance <= 4000)
+          {
+          $modifier = 0.07;
+          } */
+        $modifier = 0.03;
+        $remainder = get_remainder($distance);
+
+
+
+
+        if ($position == 1) {
+
+            if ($distance < $orgdistance) {
+
+                $newtime = win_rounded_up($time, $length, $modifier, $remainder);
+            } else {
+                $newtime = win_rounded_down($time, $length, $modifier, $remainder);
             }
+        } else {
+            if ($distance < $orgdistance) {
+                $newtime = loses_rounded_up($time, $length, $modifier, $remainder);
+            } else {
+                $newtime = loses_rounded_down($time, $length, $modifier, $remainder);
+            }
+        }
         return $newtime;
-        
-                
-            
-            
-}
-function get_remainder($distance){
+    }
 
-    if ($distance % 10 < 5)
-		{
-			$distance -= $distance % 10;
-                       
-		}
-		else
-		{
-			$distance += (10 - ($distance % 10));
-                       
-		}
-	       
-		if ($distance % 100 < 50)
-		{
-			$reminder_distance = $distance % 100;
-			
-                       
-		}
-		else
-		{
-			$reminder_distance = (100 - ($distance % 100));
-			
-                        
-		}
-                $reminder = $reminder_distance;
-                return $reminder;
-}
+    function get_remainder($distance) {
 
- //if horse wins   
-function win_rounded_up($time,$length,$modifier,$remainder){
+        if ($distance % 10 < 5) {
+            $distance -= $distance % 10;
+        } else {
+            $distance += (10 - ($distance % 10));
+        }
 
-    $newtime =  $time+(0.0007*$remainder);
-    return $newtime;
-}
- //if horse wins  
-function win_rounded_down($time,$length,$modifier,$remainder){
- 
-        $newtime =  $time-(0.0007*$remainder);
-    return $newtime;
-    
-}
- //if horse loses  
-function loses_rounded_up($time,$length,$modifier,$remainder){
-    //time+(length*modifier)-(0.0007*$remainder);
-  
-        $newtime =  $time+($length*$modifier)+(0.0007*$remainder);
-    return $newtime;
-}
- //if horse loses  
-function loses_rounded_down($time,$length,$modifier,$remainder){
+        if ($distance % 100 < 50) {
+            $reminder_distance = $distance % 100;
+        } else {
+            $reminder_distance = (100 - ($distance % 100));
+        }
+        $reminder = $reminder_distance;
+        return $reminder;
+    }
 
-     $newtime =  $time+($length*$modifier)-(0.0007*$remainder);
-    return $newtime;
-}
+    //if horse wins   
+    function win_rounded_up($time, $length, $modifier, $remainder) {
 
-?>
+        $newtime = $time + (0.0007 * $remainder);
+        return $newtime;
+    }
+
+    //if horse wins  
+    function win_rounded_down($time, $length, $modifier, $remainder) {
+
+        $newtime = $time - (0.0007 * $remainder);
+        return $newtime;
+    }
+
+    //if horse loses  
+    function loses_rounded_up($time, $length, $modifier, $remainder) {
+        //time+(length*modifier)-(0.0007*$remainder);
+
+        $newtime = $time + ($length * $modifier) + (0.0007 * $remainder);
+        return $newtime;
+    }
+
+    //if horse loses  
+    function loses_rounded_down($time, $length, $modifier, $remainder) {
+
+        $newtime = $time + ($length * $modifier) - (0.0007 * $remainder);
+        return $newtime;
+    }
+    ?>
---
+++ races.php
@@ -0,0 +1,94 @@
+<?php
+include('constant.php');
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$race_id = $_REQUEST['race_id'];
+
+$sql = "SELECT *  FROM results LEFT JOIN races ON races.race_id = results.race_id WHERE results.race_id = " . $race_id;
+
+$result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Horses Data</title>
+        <link rel="stylesheet" id="font-awesome-style-css" href="http://phpflow.com/code/css/bootstrap3.min.css" type="text/css" media="all">
+        <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css"/>
+
+        <script type="text/javascript" src="https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/1.0.3/css/dataTables.responsive.css">
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/1.0.3/js/dataTables.responsive.js"></script>
+
+        <link rel="stylesheet" id="main-css" href="main.css" type="text/css" media="all">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <ul>
+        <li><a href="meeting.php">Home</a></li>
+        <li><a href="result.php" class="active">Reults</a></li>
+        <li><a href="meeting.php" >Meetings</a></li>
+
+    </ul>
+
+    <div class="container">
+        <div class="">
+            <h1>Race Results</h1>
+            <div class="">
+                <table id="employee_grid" class="display" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Position</th>
+                            <th>Horse Name</th>
+                            <th>Distance</th>
+                            <th>Event</th>
+                            <th>Race Name</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            while ($row = $result->fetch_assoc()) {
+                                echo "<tr>"
+                                . "<td>" . $row["position"] . "</td>"
+                                . "<td>" . $row["horse"] . "</td>"
+                                . "<td>" . $row["distance"] . "</td>"
+                                . "<td>" . $row["event"] . "</td>"
+                                . "<td><a href='horses.php?rd=" . $row['distance'] . "&raceid=" . $row['race_id'] . "&meetingid=" . $row['meeting_id'] . "&mname=" . $row['race_title'] . "'>" . $row["race_title"] . "</a></td>"
+                                . "</tr>";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                        $conn->close();
+                        ?>
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+
+    </div>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#employee_grid').DataTable({
+
+                "responsive": true,
+            });
+        });
+    </script>
+
