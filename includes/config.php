3b343313477f9557e3ccdca207e9ea323c11e9a5
idzhalalov <idzhalalov@gmail.com>
2019-03-15 00:04:42 +0500

    Config: constants were defined, debugging level lean on constants values

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,6 @@
 <?php
+date_default_timezone_set("UTC");
+
 $servername = "localhost";
 $username = "bettinga";
 $password = "Newcar888!!";
@@ -7,13 +9,17 @@ $dbname = "bettinganewdb";
 // Create connection
 $mysqli = new mysqli($servername, $username, $password, $dbname);
 
-// Turn on debugging
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 // Logging
-date_default_timezone_set("UTC");
 require_once 'logging.php';
-define('LOG_LEVEL', 'ERROR');
-$logger = new logger('logs_default_algorithm.txt', LOG_LEVEL);
+define('LOG_LEVEL', 'error');
+$logger = new logger('logs.txt', LOG_LEVEL);
 
+define('APP_ROOT', realpath(__DIR__ . '/..'));
+define('WORKERS_COUNT', 2);
+define('ERROR_REPORTING', E_ALL);
+define('DISPLAY_ERRORS', 1);
+define('MODIFIER', '0.025');
+
+// Turn on debugging
+error_reporting(ERROR_REPORTING);
+ini_set('display_errors', DISPLAY_ERRORS);
