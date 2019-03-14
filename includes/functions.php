113c1230853745cffa612f9653e6ff2f59159af7
idzhalalov <idzhalalov@gmail.com>
2019-03-15 00:01:18 +0500

    arrayGet() function was added, the whole code was reformatted

--- includes/functions.php
+++ includes/functions.php
@@ -1,43 +1,68 @@
 <?php
-define("MODIFIER",'0.025');
-function get_rows($table_and_query) {
-	global $mysqli;
-	$total = $mysqli->query("SELECT * FROM $table_and_query");
-	$total = $total->num_rows;
-	return $total;
+
+function get_rows($table_and_query)
+{
+    global $mysqli;
+    $total = $mysqli->query("SELECT * FROM $table_and_query");
+    $total = $total->num_rows;
+
+    return $total;
 }
 
-function meeting_details($mid) {
-	global $mysqli;
-	$meetingdet = $mysqli->query("SELECT * FROM `tbl_meetings` WHERE `meeting_id`='$mid'");
-	$detail = $meetingdet->fetch_object();
-	return $detail;
-}
-function horse_details($hid) {
-	global $mysqli;
-	$horsedet = $mysqli->query("SELECT * FROM `tbl_horses` WHERE `horse_id`='$hid'");
-	$detail = $horsedet->fetch_object();
-	return $detail;
-}
-function race_details($rid) {
-	global $mysqli;
-	$racedet = $mysqli->query("SELECT * FROM `tbl_races` WHERE `race_id`='$rid'");
-	$detail = $racedet->fetch_object();
-	return $detail;
-}
-function get_horses_in_race($rid) {
-	global $mysqli;
-	$racedet = $mysqli->query("SELECT * FROM `tbl_races` WHERE `race_id`='$rid'");
-	$detail = $racedet->fetch_object();
-	return $detail;
-}
-function result_details($rid, $hid) {
-	global $mysqli;
-	$resdet = $mysqli->query("SELECT * FROM `tbl_results` WHERE `race_id`='$rid' AND `horse_id`='$hid'");
-	$detail = $resdet->fetch_object();
-	return $detail;
+function meeting_details($mid)
+{
+    global $mysqli;
+    $meetingdet
+        = $mysqli->query("SELECT * FROM `tbl_meetings` WHERE `meeting_id`='$mid'");
+    $detail = $meetingdet->fetch_object();
+
+    return $detail;
 }
 
+function horse_details($hid)
+{
+    global $mysqli;
+    $horsedet
+        = $mysqli->query("SELECT * FROM `tbl_horses` WHERE `horse_id`='$hid'");
+    $detail = $horsedet->fetch_object();
+
+    return $detail;
+}
+
+function race_details($rid)
+{
+    global $mysqli;
+    $racedet
+        = $mysqli->query("SELECT * FROM `tbl_races` WHERE `race_id`='$rid'");
+    $detail = $racedet->fetch_object();
+
+    return $detail;
+}
+
+function get_horses_in_race($rid)
+{
+    global $mysqli;
+    $racedet
+        = $mysqli->query("SELECT * FROM `tbl_races` WHERE `race_id`='$rid'");
+    $detail = $racedet->fetch_object();
+
+    return $detail;
+}
+
+function result_details($rid, $hid)
+{
+    global $mysqli;
+    $resdet
+        = $mysqli->query("SELECT * FROM `tbl_results` WHERE `race_id`='$rid' AND `horse_id`='$hid'");
+    $detail = $resdet->fetch_object();
+
+    return $detail;
+}
+
+function arrayGet(array $data, $key, $defaultValue = null)
+{
+    return (isset($data[$key])) ? $data[$key] : $defaultValue;
+}
 /*	
 	function get_user_details($user_name) {
 		global $mysqli;
@@ -85,4 +110,3 @@ function result_details($rid, $hid) {
 		return $lastuser->id;
 	}
 	*/
-?>
